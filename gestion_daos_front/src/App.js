50779cff13e6f9d30f2a79fb5105220ac3c8e619
eadarak <elabdoudrame2001@gmail.com>
2024-02-19 01:38:26 +0000

    Initialisation du front

---
+++ gestion_daos_front/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import SidebarComp from './components/SidebarComp';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <SidebarComp />
+    </div>
+  );
+}
+
+export default App;
