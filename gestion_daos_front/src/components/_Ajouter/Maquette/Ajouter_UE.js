bf838e3efcf603ac94fa2929d2777016da18a944
eadarak <elabdoudrame2001@gmail.com>
2024-03-04 06:07:51 +0000

    je sais pas

--- gestion_daos_front/src/components/_Ajouter/Maquette/Ajouter_UE.js
+++ gestion_daos_front/src/components/_Ajouter/Maquette/Ajouter_UE.js
@@ -3,9 +3,59 @@ import Modal from '@mui/joy/Modal';
 import Typography from '@mui/material/Typography';
 import LibraryAddIcon from '@mui/icons-material/LibraryAdd';
 import { Box, DialogContent, DialogTitle, Divider, Stack, TextField, Grid, Button} from '@mui/material';
+import { MAQUETTE_URL } from '../../../Server_URL/Urls';
+import ListeUE from '../../_Listes/Maquette/ListeUE';
 
 function Ajouter_UE () {
     const [open, setOpen] = React.useState(false);
+    const initialUE = {
+        idUE: 0,
+        libelleUE: '',
+        codeUE: '',
+        creditUE: 0,
+        coefficientUE: 0,
+        descriptionUE: ''
+    }
+
+    const [data , setData] = React.useState(initialUE);
+
+    const handleChange = (e) =>{
+       setData({
+        ...data,
+        [e.target.id] : e.target.value
+       })
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        fetch(`${MAQUETTE_URL}ue`,{
+            method: 'POST',
+            headers: {'Content-Type' : 'application/json'},
+            body : JSON.stringify(data)
+        })
+        .then(response =>{
+            if(!response.ok){
+                console.log('la reponse est different de ok')
+            }
+            return response.json()
+        })
+        .then( data => {
+            console.log( 'un nouveau UE a ete ajoutee');
+            console.log(data);
+
+            //Reinitialisation du formulaire
+            setData(initialUE);
+
+            //rediriger vers la page des listes des UEs
+            window.location.href = 'ListeUE'
+
+        })
+        .catch( err => {
+            console.log('Une Erreur est survenu lors de l\'operation de POST' ,err);
+        })
+
+    }
 
     return (
         <React.Fragment>
@@ -35,6 +85,8 @@ function Ajouter_UE () {
                                 <TextField
                                     id="libelleUE"
                                     label="Libelle UE"
+                                    value={data.libelleUE}
+                                    onChange={handleChange}
                                     required
                                     fullWidth
                                 />
@@ -43,6 +95,9 @@ function Ajouter_UE () {
                                 <TextField
                                     id="codeUE"
                                     label="Code UE"
+                                    value={data.codeUE}
+                                    onChange={handleChange}
+
                                     fullWidth
                                     required
                                 />
@@ -51,6 +106,9 @@ function Ajouter_UE () {
                                 <TextField
                                     id="creditUE"
                                     label="Nombre de Credit"
+                                    value={data.creditUE}
+                                    onChange={handleChange}
+
                                     fullWidth
                                     required
                                 />
@@ -59,6 +117,9 @@ function Ajouter_UE () {
                                 <TextField
                                     id="coefficientUE"
                                     label="Coefficient"
+                                    value={data.coefficientUE}
+                                    onChange={handleChange}
+
                                     fullWidth
                                     required
                                 />
@@ -67,6 +128,8 @@ function Ajouter_UE () {
                                 <TextField
                                     id="descriptionUE"
                                     label="description UE"
+                                    value={data.descriptionUE}
+                                    onChange={handleChange}
                                     variant="filled"
                                     fullWidth
                                     multiline
@@ -82,7 +145,15 @@ function Ajouter_UE () {
                             '&:hover': { backgroundColor: '#000',
                                         color:'white' }
                         }}>Annuler</Button>
-                        <Button variant="contained"  sx={{ background: ' rgb(9, 44, 38)' , '&:hover': { backgroundColor: 'rgb(17, 77, 67)' }}} className='validButton' id='validButton'>Enregistrer</Button>
+                        <Button 
+                            variant="contained"  
+                            sx={{ background: ' rgb(9, 44, 38)' , '&:hover': { backgroundColor: 'rgb(17, 77, 67)' }}} 
+                            className='validButton' 
+                            id='validButton'
+                            onClick={handleSubmit}
+                        >
+                            Enregistrer
+                        </Button>
                     </Box>
                 </Box>
             </Modal>
